b07904f55b265303835f34d1388a1bae19034a5f
KhamzinaOO <shinni.akiba@gmail.com>
2024-07-03 16:46:32 +0300

    Update TaskListScreen.kt

--- app/src/main/java/com/example/taskmanagersummerpractice/view/TaskListScreen.kt
+++ app/src/main/java/com/example/taskmanagersummerpractice/view/TaskListScreen.kt
@@ -35,14 +35,12 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
-import androidx.compose.ui.BiasAlignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.taskmanagersummerpractice.R
 import com.example.taskmanagersummerpractice.model.Task
